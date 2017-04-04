48d4f7cce61acc4765c4d2cc813813515200283a
LarsM <lam@cphbusiness.dk>
2017-04-04 12:48:49 +0200

    remove warning message due to useStrict

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 import auth from '../authorization/auth'
 import { observer } from "mobx-react";
 
-import {useStrict} from "..//stores/useStrict"
+import "..//stores/useStrict";
 
 const App = observer(class App extends Component {
 
